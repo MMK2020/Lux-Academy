694efc543721616aa3b86e366a972c3c43818db2
MMK2020 <82337038+MMK2020@users.noreply.github.com>
2022-03-02 21:04:35 +0300

    Add files via upload

---
+++ javascript/js-youtube-downloader/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const ytdl = require("ytdl-core");
+app.set("view engine", "ejs");
+// OUR ROUTES WILL GO HERE
+app.listen(3000, () => {
+	console.log("Express Web Server is running on http://localhost:3000");
+});
+
+app.get("/", (req, res) => {
+	return res.render("index");
+});
+
+app.get('\download' , (req, res) => {
+    const v_id = req.query.url.split('v=')[1];
+    return res.render('download', {
+        url: "https://www.youtube.com/embed/" + v_id,
+    });
+});
+
+app.get("/download", async (req, res) => {
+	const v_id = req.query.url.split('v=')[1];
+    const info = await ytdl.getInfo(req.query.url);
+	return res.render("download", {
+		url: "https://www.youtube.com/embed/" + v_id,
+        info: info.formats.sort((a, b) => {
+            return a.mimeType < b.mimeType;
+        }),
+	});
+});
